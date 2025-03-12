2937e3dd90b3a2a43da554b236dbc4915375e177
SonalKhare <sonalkhare.sk215@gmail.com>
2025-03-12 22:31:09 +1100

    comments commit

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -20,3 +20,22 @@ export const fetchPostsByTopicSlug = (slug: string): Promise<PostWithData[]> => 
     }
   })
 }
+
+export const fetchTopPosts = async (): Promise<PostWithData[]> => {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: 'desc'
+        }
+      }
+    ],
+    include: {
+      topic: {select: {slug: true}},
+      user: {select: {name: true, image: true}},
+      _count: {select: {comments: true}},
+    },
+    take: 5
+  })
+
+}
